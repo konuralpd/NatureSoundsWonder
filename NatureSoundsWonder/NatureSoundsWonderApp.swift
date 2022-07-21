f32dbda34852a02900bbd83b8b70331a65598125
konuralpd <98783085+konuralpd@users.noreply.github.com>
2022-07-21 07:15:56 +0300

    Initial Commit

---
+++ NatureSoundsWonder/NatureSoundsWonderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatureSoundsWonderApp.swift
+//  NatureSoundsWonder
+//
+//  Created by Mac on 21.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct NatureSoundsWonderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
